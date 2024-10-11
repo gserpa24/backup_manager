9c069238569363131ae889012725291b3978b7a7
Gustavo Serpa <gusserpa02@gmail.com>
2024-10-11 17:46:14 -0500

    front base

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExportVM from './comprimir';
+
+function App() {
+  return (
+    <div className="App">
+      <Comprimir />
+    </div>
+  );
+}
+
+export default App;
