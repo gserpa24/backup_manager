f89e3c8f7dfc3d3c3e408b11696a50046f4da6b9
Gustavo Serpa <gusserpa02@gmail.com>
2024-10-11 20:15:50 -0500

    Add login app

---
+++ backend/accounts/forms.py
@@ -0,0 +1,15 @@
+# accounts/forms.py
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class CustomAuthenticationForm(forms.Form):
+    username = forms.CharField(label='Usuario', max_length=150)
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
---
+++ backend/accounts/views.py
@@ -0,0 +1,36 @@
+# accounts/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirigir al login después de registrarse
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Cambia 'home' por la vista a la que deseas redirigir
+    else:
+        form = CustomAuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+@login_required
+def home_view(request):
+    return render(request, 'accounts/home.html')  # Crea una plantilla home.html
