76912c3ae2b956d63bf51c3d8e89467a31724c72
Gustavo Serpa <gusserpa02@gmail.com>
2024-10-18 23:56:37 -0500

    +Salida por consola

--- tasks/views.py
+++ tasks/views.py
@@ -21,22 +21,30 @@ def script_view(request):
     output = ''
     error = ''
 
-    # Ruta del script
-    script_path = os.path.join('tasks', 'scripts', 'comprimir.bat')
+    # Diccionario para mapear nombres de scripts a sus rutas
+    scripts = {
+        'script1': os.path.join('tasks', 'scripts', 'comprimir.bat'),
+        'script2': os.path.join('tasks', 'scripts', 'script2.bat'),
+        'script3': os.path.join('tasks', 'scripts', 'script3.bat'),
+    }
 
     if request.method == 'POST':
-        try:
-            # Ejecutar el script y capturar la salida
-            result = subprocess.run(['cmd', '/c', script_path],
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE,
-                                    text=True,
-                                    shell=True,
-                                    check=True)
-            output = result.stdout
-            error = result.stderr
-        except subprocess.CalledProcessError as e:
-            output = e.stdout
-            error = e.stderr
+        script_name = request.POST.get('script_name')
+        script_path = scripts.get(script_name)
 
-    return render(request, 'mi_template.html', {'output': output, 'error': error})
+        if script_path:
+            try:
+                # Ejecutar el script y capturar la salida
+                result = subprocess.run(['cmd', '/c', script_path],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE,
+                                        text=True,
+                                        shell=True,
+                                        check=True)
+                output = result.stdout
+                error = result.stderr
+            except subprocess.CalledProcessError as e:
+                output = e.stdout
+                error = e.stderr
+
+    return render(request, 'mi_template.html', {'output': output, 'error': error, 'scripts': scripts})
