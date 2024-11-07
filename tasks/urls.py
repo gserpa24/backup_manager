e129ee5776b522523f76cf663fa87cb5a4109fba
Gustavo Serpa <gusserpa02@gmail.com>
2024-11-07 11:46:35 -0500

    vms views update

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,6 @@ from .views import home, list_vms_view, execute_vm_script
 
 urlpatterns = [
     path('home/', home, name='home'),
-    path('listar-vms/', list_vms_view, name='list_vms'),
+    path('vms/', list_vms_view, name='list_vms'),
     path('execute_vm_script/<str:vm>/', execute_vm_script, name='execute_vm_script'),
 ]
