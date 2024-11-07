76cfc0efd7820d098e4af134831ecb8116ceaa81
Gustavo Serpa <gusserpa02@gmail.com>
2024-11-07 10:45:43 -0500

    Add Backup model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Backup(models.Model):
+    vm_name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    backup_date = models.DateTimeField(auto_now_add=True)
+    execution_time = models.DurationField()
+    vm_size = models.BigIntegerField()
+    backup_file_path = models.CharField(max_length=500)
+    success = models.BooleanField(default=True)
+    error_message = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Backup de {self.vm_name} - {self.backup_date.strftime('%Y-%m-%d %H:%M:%S')}"
