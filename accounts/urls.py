879a8a61f3a80f848d108bca7c05f96461c5dfe1
Gustavo Serpa <gusserpa02@gmail.com>
2024-11-11 11:05:59 -0500

    login update

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import login_view, logout_view
 
 urlpatterns = [
-    path('', login_view, name='login'),  # Ruta para login
+    path('login/', login_view, name='login'),  # Ruta para login
     path('logout/', logout_view, name='logout'),
 ]
 
